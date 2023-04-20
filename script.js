78011b3ac904cc441f8ddb2602b7601cc5fbdc4e
TN <tn@hotmail.com>
2023-04-20 04:44:21 +0200

    Globally: added favicon Flower's layout: changes in margins and text alignment + some other minor changes, like labels. Functions: added parameters (min & max) to  getRandomNumber to have more control on range of the generated numbers. This will allow the function to be more useful and universal:

--- script.js
+++ script.js
@@ -1,23 +1,27 @@
+// loading elements
 const badge = document.querySelector(".badge");
 const title = document.querySelector(".title");
 const colorCode = document.querySelector(".colorCode");
 
+// Events
 badge.addEventListener("click", () => {
   title.style.color = getRandomColor();
 });
 
-// generate random color
-function getRandomNumber() {
-  return Math.floor(Math.random() * 16);
+// Functions
+// generate a random number based on the given range [min ... max]
+function getRandomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
+// generate a random color
 function getRandomColor() {
   const hex = "0123456789ABCDEF";
   let color = "#";
   for (let i = 0; i < 6; i++) {
-    color += hex[getRandomNumber()];
+    color += hex[getRandomNumber(0, 15)];
   }
-  console.log(color);
+  console.log("Color's code:", color);
   colorCode.textContent = color;
   return color;
 }
